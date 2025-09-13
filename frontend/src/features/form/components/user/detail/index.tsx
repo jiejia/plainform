b561dd86f9824d212b897cb402e315a70b539174
jiejia2009 <jiejia2009@gmail.com>
2025-09-13 10:32:27 +0800

    refactor Detail component: enhance layout and display form fields in a structured list

--- frontend/src/features/form/components/user/detail/index.tsx
+++ frontend/src/features/form/components/user/detail/index.tsx
@@ -1,21 +1,32 @@
 'use client'
 
-
 import { Form as FormType } from "@/features/form/types/form";
 import { Card, CardBody, CardHeader, CardFooter } from "@heroui/react";
-
+import { Field } from "@/features/form/types/field";
 export default function Detail({ form }: { form: FormType }) {
 
     console.log(form.fields);
     return (
-        <div className="py-4 px-4 min-h-screen">
-            <Card>
-                   <CardHeader className="flex flex-col gap-2">
-                        <h2 className="text-2xl font-bold">{form.title}</h2>
-                    </CardHeader> 
-                    <CardBody>
+        <div className="py-4 px-4 h-screen">
+            <Card className="h-full mx-auto max-w-5xl">
+                <CardHeader className="flex flex-col gap-2">
+                    <h2 className="text-2xl font-bold">{form.title}</h2>
+                </CardHeader>
+                <CardBody>
+                    <ul className="grid grid-flow-row gap-2">
+                        {
+                            form.fields?.map((field: Field, index: number) => (
+                                <li key={index}>
+                                    <span>{index + 1}. {field.title}</span>
+                                    <div>
 
-                    </CardBody>
+                                    </div>
+                                </li>
+                            ))
+                        }
+                    </ul>
+
+                </CardBody>
 
             </Card>
 
