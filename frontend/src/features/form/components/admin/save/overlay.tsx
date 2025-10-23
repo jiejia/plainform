18eda180c4a1bcabac6324809499887027741fb6
jiejia2009 <jiejia2009@gmail.com>
2025-10-23 16:13:43 +0800

    feat(i18n): enhance localization in overlay component by updating control and field names with translation keys

--- frontend/src/features/form/components/admin/save/overlay.tsx
+++ frontend/src/features/form/components/admin/save/overlay.tsx
@@ -8,8 +8,10 @@ import { DragOverlay } from "@dnd-kit/core";
 import { Item } from "@/features/core/components/shared/item";
 import clsx from "clsx";
 import * as Icons from 'lucide-react'
+import { useTranslations } from 'next-intl';
 
 export default function Overlay({ activeItem, fields, controls }: { activeItem: DraggableItem, fields: Field[], controls: Control[] }) {
+    const t = useTranslations();
 
     // get control icon component
     let Icon = Icons.Circle;
@@ -25,13 +27,13 @@ export default function Overlay({ activeItem, fields, controls }: { activeItem: 
                         <Item
                             className="flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-50 cursor-pointer border border-gray-400 transition-colors">
                             <Icon size={18} className="text-blue-500" />
-                            <span className="text-sm font-medium">{controls[activeItem.id].name}</span>
+                            <span className="text-sm font-medium">{t(`form.${controls[activeItem.id].type}`)}</span>
                         </Item>
                     )
                     : fields[activeItem.id] && (
                         <Item className={clsx("flex items-center justify-between p-3 rounded-lg border border-gray-400 hover:bg-gray-100 transition-colors", { "outline -outline-offset-2 outline-2 outline-primary": fields[activeItem.id].active })}>
                             <span className="text-sm font-medium text-gray-700">{fields[activeItem.id].title}</span>
-                            <span className="text-xs text-gray-500 px-2 py-1 rounded-full">{fields[activeItem.id].control_name}</span>
+                            <span className="text-xs text-gray-500 px-2 py-1 rounded-full">{t(`form.${fields[activeItem.id].control_type}`)}</span>
                         </Item>
                     ))}
         </DragOverlay>
