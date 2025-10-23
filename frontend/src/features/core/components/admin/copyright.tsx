35b1bdf874a54a05281332b9826a4ff3adbb0dea
jiejia2009 <jiejia2009@gmail.com>
2025-10-23 16:23:41 +0800

    feat(i18n): enhance localization in copyright component by adding translation keys for powered by message

--- frontend/src/features/core/components/admin/copyright.tsx
+++ frontend/src/features/core/components/admin/copyright.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import { Github } from "lucide-react";
+import { useTranslations } from 'next-intl';
 
 export default function Copyright() {
+
+    const t = useTranslations();
     const startYear = 2025;
     const currentYear = new Date().getFullYear();
     const yearDisplay = currentYear > startYear ? `${startYear} - ${currentYear}` : startYear.toString();
 
     return (
-        <p className="text-xs text-center mt-4 w-full">© {yearDisplay} Powered By PlainForm <Link
+        <p className="text-xs text-center mt-4 w-full">© {yearDisplay} {t('core.powered_by', { app_name: "PlainForm" })} <Link
             href={"https://github.com/jiejia/plainform"}><Github size={15} className="inline"/></Link>
         </p>
     )
